871420a9cf7935813a19c9225a448f767a118054
iDerra <victordosantosluis@gmail.com>
2025-03-14 09:18:45 +0100

    Docs

--- PlantApp/src/FavouritesScreen.tsx
+++ PlantApp/src/FavouritesScreen.tsx
@@ -1,15 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  FlatList,
-  Image,
-  Dimensions,
-  TouchableOpacity,
-  TextInput,
-  Modal,
-  Alert
-} from 'react-native';
+import { View, Text, FlatList, Image, Dimensions, TouchableOpacity, TextInput, Modal, Alert } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
